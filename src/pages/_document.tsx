ad2d0a9f2c066ae442f322d02c2b12978a5af342
Erick Peña <erickpea29@gmail.com>
2025-02-19 09:58:55 -0600

    Tawk test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,28 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
       <Head />
-      <body className="antialiased">
+      <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/67b5fb2e6cffb3190ee1ef96/1ikfdqb9s';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </Html>
   );
